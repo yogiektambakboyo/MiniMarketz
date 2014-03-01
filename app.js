3d8ad490686db5cd43e2bdd0b7e78a6d1bd1d387
Yogi Aditya <yogiektambakboyo@gmail.com>
2014-03-01 10:40:20 +0700

    upload Heroku

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var mongoose = require('mongoose'),
     bcrypt = require('bcrypt'),
     SALT_WORK_FACTOR = 10;
 var db;
-var uri = 'mongodb://127.0.0.1:27017/angularapp';
-//var uri = 'mongodb://yogiaditya:angularappdb@ds061518.mongolab.com:61518/angularapp';
+//var uri = 'mongodb://127.0.0.1:27017/angularapp';
+var uri = 'mongodb://yogiaditya:angularappdb@ds061518.mongolab.com:61518/angularapp';
 
 
 db = mongoose.createConnection(uri);
